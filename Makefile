b92ef0489d7357f0850ede53de4807d3b41fd1b5
Ben Schwartz <benschw@gmail.com>
2014-12-04 17:30:15 -0600

    refactor(satis): rearranging, adding tests

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 default: build
 
 clean:
+	rm -rf web
 	rm -rf satisapi-go
 	rm -rf test-config.json
 	rm -rf satis
@@ -8,8 +9,9 @@ clean:
 
 deps:
 	go get
+	go get github.com/gorilla/http
 
 satis:
 	curl -sS https://getcomposer.org/installer | php
-	php ./composer.phar create-project composer/satis --stability=dev --keep-vcs
+	php ./composer.phar create-project composer/satis lib/satis --stability=dev --keep-vcs
 
--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
-	"github.com/benschw/satisapi-go/satisapi"
+	"github.com/benschw/satisapi-go/satis"
 	"log"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-	s := &satisapi.Server{}
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := &satis.Server{
+		DbPath:    dir + "/db.json",
+		WebPath:   dir + "/web/",
+		SatisPath: dir + "/lib/satis/",
+		Bind:      ":8080",
+		Name:      "My Repo",
+		Homepage:  "http://localhost:8080",
+	}
+
 	if err := s.Run(); err != nil {
 		log.Fatal(err)
 	}
